03adec408f472a2e6d47542879321b184aa4fd58
George B. <george@octelium.com>
2025-05-21 19:04:22 +0000

    cluster/nocturne - add tests for container env

--- cluster/nocturne/nocturne/controllers/services/k8s_upstream_test.go
+++ cluster/nocturne/nocturne/controllers/services/k8s_upstream_test.go
@@ -18,6 +18,7 @@ package svccontroller
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
@@ -64,6 +65,14 @@ func TestSetK8sUpstream(t *testing.T) {
 						Container: &corev1.Service_Spec_Config_Upstream_Container{
 							Port:  8080,
 							Image: "nginx:latest",
+							Env: []*corev1.Service_Spec_Config_Upstream_Container_Env{
+								{
+									Name: utilrand.GetRandomStringCanonical(8),
+									Type: &corev1.Service_Spec_Config_Upstream_Container_Env_Value{
+										Value: utilrand.GetRandomString(32),
+									},
+								},
+							},
 						},
 					},
 				},
@@ -84,6 +93,89 @@ func TestSetK8sUpstream(t *testing.T) {
 
 	assert.Equal(t, 1, len(dep.Spec.Template.Spec.Containers))
 	assert.True(t, len(dep.Spec.Template.Spec.Containers[0].Name) > 0)
+
+	assert.Equal(t, svc.Spec.Config.Upstream.GetContainer().Env[0].Name, dep.Spec.Template.Spec.Containers[0].Env[0].Name)
+	assert.Equal(t, svc.Spec.Config.Upstream.GetContainer().Env[0].GetValue(), dep.Spec.Template.Spec.Containers[0].Env[0].Value)
+	err = c.OnDelete(ctx, svcV)
+	assert.Nil(t, err)
+}
+
+func TestSetK8sUpstreamEnvFromSecret(t *testing.T) {
+
+	ctx := context.Background()
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	fakeC := tst.C
+
+	adminSrv := admin.NewServer(&admin.Opts{
+		OcteliumC:  fakeC.OcteliumC,
+		IsEmbedded: true,
+	})
+
+	os.Setenv("OCTELIUM_REGION_NAME", "default")
+	defer os.Unsetenv("OCTELIUM_REGION_NAME")
+
+	sec, err := fakeC.OcteliumC.CoreC().CreateSecret(ctx, &corev1.Secret{
+		Metadata: &metav1.Metadata{
+			Name: utilrand.GetRandomStringCanonical(8),
+		},
+		Spec:   &corev1.Secret_Spec{},
+		Status: &corev1.Secret_Status{},
+		Data: &corev1.Secret_Data{
+			Type: &corev1.Secret_Data_Value{
+				Value: utilrand.GetRandomString(32),
+			},
+		},
+	})
+	assert.Nil(t, err)
+
+	c := NewController(fakeC.OcteliumC, fakeC.K8sC)
+	svc, err := adminSrv.CreateService(ctx, &corev1.Service{
+		Metadata: &metav1.Metadata{
+			Name: utilrand.GetRandomStringCanonical(8),
+		},
+		Spec: &corev1.Service_Spec{
+			Mode: corev1.Service_Spec_HTTP,
+			Config: &corev1.Service_Spec_Config{
+				Upstream: &corev1.Service_Spec_Config_Upstream{
+					Type: &corev1.Service_Spec_Config_Upstream_Container_{
+						Container: &corev1.Service_Spec_Config_Upstream_Container{
+							Port:  8080,
+							Image: "nginx:latest",
+
+							Env: []*corev1.Service_Spec_Config_Upstream_Container_Env{
+								{
+									Name: "key1",
+									Type: &corev1.Service_Spec_Config_Upstream_Container_Env_FromSecret{
+										FromSecret: sec.Metadata.Name,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+
+	assert.Nil(t, err, "%+v", err)
+
+	svcV, err := fakeC.OcteliumC.CoreC().GetService(ctx, &rmetav1.GetOptions{Uid: svc.Metadata.Uid})
+	assert.Nil(t, err)
+
+	err = c.OnAdd(ctx, svcV)
+	assert.Nil(t, err)
+
+	dep, err := fakeC.K8sC.AppsV1().Deployments(ns).Get(ctx, k8sutils.GetSvcK8sUpstreamHostname(svcV, "default"), k8smetav1.GetOptions{})
+	assert.Nil(t, err)
+
+	assert.Equal(t, 1, len(dep.Spec.Template.Spec.Containers))
+	assert.True(t, len(dep.Spec.Template.Spec.Containers[0].Name) > 0)
+	assert.Equal(t, dep.Spec.Template.Spec.Containers[0].Env[0].ValueFrom.SecretKeyRef.Name, fmt.Sprintf("svc-env-%s-%s", svc.Metadata.Uid, sec.Metadata.Uid))
 
 	err = c.OnDelete(ctx, svcV)
 	assert.Nil(t, err)
