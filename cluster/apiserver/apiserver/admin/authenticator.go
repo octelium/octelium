e20c862f5a13eec34570980160f165df46c655c7
George B. <george@octelium.com>
2025-06-04 19:27:54 +0000

    cluster/apiserver - add Authenticator methods

---
+++ cluster/apiserver/apiserver/admin/authenticator.go
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020-present Octelium. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3,
+ * as published by the Free Software Foundation of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package admin
+
+import (
+	"context"
+
+	"github.com/octelium/octelium/apis/main/corev1"
+	"github.com/octelium/octelium/apis/main/metav1"
+	"github.com/octelium/octelium/apis/rsc/rmetav1"
+	"github.com/octelium/octelium/cluster/apiserver/apiserver/serr"
+	"github.com/octelium/octelium/cluster/common/apivalidation"
+	"github.com/octelium/octelium/cluster/common/urscsrv"
+)
+
+func (s *Server) ListAuthenticator(ctx context.Context, req *corev1.ListAuthenticatorOptions) (*corev1.AuthenticatorList, error) {
+
+	vAuthenticators, err := s.octeliumC.CoreC().ListAuthenticator(ctx, urscsrv.GetPublicListOptions(req))
+	if err != nil {
+		return nil, serr.InternalWithErr(err)
+	}
+
+	return vAuthenticators, nil
+}
+
+func (s *Server) DeleteAuthenticator(ctx context.Context, req *metav1.DeleteOptions) (*metav1.OperationResult, error) {
+	if err := apivalidation.CheckDeleteOptions(req, nil); err != nil {
+		return nil, err
+	}
+
+	dev, err := s.octeliumC.CoreC().GetAuthenticator(ctx, &rmetav1.GetOptions{
+		Name: req.Name,
+		Uid:  req.Uid,
+	})
+	if err != nil {
+		return nil, serr.K8sNotFoundOrInternalWithErr(err)
+	}
+
+	if _, err := s.octeliumC.CoreC().DeleteAuthenticator(ctx, &rmetav1.DeleteOptions{Uid: dev.Metadata.Uid}); err != nil {
+		return nil, serr.InternalWithErr(err)
+	}
+
+	return &metav1.OperationResult{}, nil
+}
+
+func (s *Server) GetAuthenticator(ctx context.Context, req *metav1.GetOptions) (*corev1.Authenticator, error) {
+	if err := apivalidation.CheckGetOptions(req, nil); err != nil {
+		return nil, err
+	}
+
+	ret, err := s.octeliumC.CoreC().GetAuthenticator(ctx, &rmetav1.GetOptions{
+		Uid:  req.Uid,
+		Name: req.Name,
+	})
+	if err != nil {
+		return nil, serr.K8sNotFoundOrInternalWithErr(err)
+	}
+
+	return ret, nil
+}
