c6e5e3c97dc71ddad2d3a81282385dddd398c741
George B. <george@octelium.com>
2025-08-16 14:58:35 +0000

    cluster/vigil - introduce direct plugin

---
+++ cluster/vigil/vigil/modes/httpg/middlewares/direct/direct.go
@@ -0,0 +1,108 @@
+/*
+ * Copyright Octelium Labs, LLC. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3,
+ * as published by the Free Software Foundation of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package direct
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/octelium/octelium/apis/main/corev1"
+	"github.com/octelium/octelium/cluster/common/celengine"
+	"github.com/octelium/octelium/cluster/vigil/vigil/modes/httpg/middlewares"
+	"github.com/octelium/octelium/cluster/vigil/vigil/modes/httpg/middlewares/commonplugin"
+)
+
+type middleware struct {
+	next      http.Handler
+	phase     corev1.Service_Spec_Config_HTTP_Plugin_Phase
+	celEngine *celengine.CELEngine
+}
+
+func New(ctx context.Context, next http.Handler, celEngine *celengine.CELEngine, phase corev1.Service_Spec_Config_HTTP_Plugin_Phase) (http.Handler, error) {
+	return &middleware{
+		next:      next,
+		phase:     phase,
+		celEngine: celEngine,
+	}, nil
+}
+
+func (m *middleware) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	ctx := req.Context()
+
+	reqCtx := middlewares.GetCtxRequestContext(ctx)
+	cfg := reqCtx.ServiceConfig
+
+	if cfg == nil || cfg.GetHttp() == nil || len(cfg.GetHttp().Plugins) == 0 {
+		m.next.ServeHTTP(rw, req)
+		return
+	}
+
+	for _, plugin := range cfg.GetHttp().Plugins {
+		switch plugin.Type.(type) {
+		case *corev1.Service_Spec_Config_HTTP_Plugin_Direct_:
+			if plugin.IsDisabled {
+				continue
+			}
+
+			if !commonplugin.MatchesPhase(plugin, m.phase) {
+				continue
+			}
+
+			if !commonplugin.ShouldEnforcePlugin(ctx, &commonplugin.ShouldEnforcePluginOpts{
+				Plugin:    plugin,
+				CELEngine: m.celEngine,
+			}) {
+				continue
+			}
+
+			direct := plugin.GetDirect()
+			isMatched, err := m.celEngine.EvalCondition(ctx, direct.Condition, map[string]any{
+				"ctx": reqCtx.ReqCtxMap,
+			})
+			if err != nil {
+				continue
+			}
+			if !isMatched {
+				continue
+			}
+
+			body := direct.Body
+
+			if body != nil && body.ContentType != "" {
+				rw.Header().Set("Content-Type", body.ContentType)
+			}
+			rw.Header().Set("Server", "octelium")
+			rw.WriteHeader(int(direct.StatusCode))
+
+			if body != nil {
+				switch body.Type.(type) {
+				case *corev1.Service_Spec_Config_HTTP_Plugin_Direct_Body_Inline:
+					rw.Write([]byte(body.GetInline()))
+				case *corev1.Service_Spec_Config_HTTP_Plugin_Direct_Body_InlineBytes:
+					rw.Write(body.GetInlineBytes())
+				}
+			}
+
+			return
+		default:
+			continue
+		}
+	}
+
+	m.next.ServeHTTP(rw, req)
+}
