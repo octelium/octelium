f2a93caa4945dbbfc40e13a5d6080356d5cd1b38
George B. <george@octelium.com>
2025-10-02 12:18:23 +0000

    cluster/vigil - add getStatusState for http metrics middleware

--- cluster/vigil/vigil/modes/httpg/middlewares/metrics/metrics.go
+++ cluster/vigil/vigil/modes/httpg/middlewares/metrics/metrics.go
@@ -17,12 +17,15 @@
 package metrics
 
 import (
+	"bufio"
 	"context"
+	"net"
 	"net/http"
 
 	"github.com/octelium/octelium/cluster/vigil/vigil/metricutils"
 	"github.com/octelium/octelium/cluster/vigil/vigil/modes/httpg/httputils"
 	"github.com/octelium/octelium/cluster/vigil/vigil/modes/httpg/middlewares"
+	"github.com/pingcap/errors"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 )
@@ -46,7 +49,11 @@ func (m *middleware) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	m.commonMetrics.AtRequestStart()
 
-	m.next.ServeHTTP(w, req)
+	crw := &responseWriter{
+		ResponseWriter: w,
+		statusCode:     200,
+	}
+	m.next.ServeHTTP(crw, req)
 
 	state := func() string {
 		switch {
@@ -65,6 +72,10 @@ func (m *middleware) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			Value: attribute.StringValue(getMethod(req)),
 		},
 		{
+			Key:   "req.http.status",
+			Value: attribute.StringValue(getStatusState(crw.statusCode)),
+		},
+		{
 			Key:   "state",
 			Value: attribute.StringValue(state),
 		},
@@ -80,4 +91,53 @@ func getMethod(req *http.Request) string {
 	default:
 		return "UNKNOWN"
 	}
+}
+
+func getStatusState(code int) string {
+	switch {
+	case code >= 200 && code < 300:
+		return "2xx"
+	case code >= 500 && code < 600:
+		return "5xx"
+	case code >= 400 && code < 500:
+		return "4xx"
+	case code >= 300 && code < 400:
+		return "3xx"
+	case code >= 100 && code < 200:
+		return "1xx"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (r *responseWriter) WriteHeader(status int) {
+	r.ResponseWriter.WriteHeader(status)
+	r.statusCode = status
+}
+
+func (w *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hj, ok := w.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, errors.Errorf("ResponseWriter is not a Hijacker")
+	}
+
+	return hj.Hijack()
+}
+
+func (r *responseWriter) Flush() {
+	if f, ok := r.ResponseWriter.(http.Flusher); ok {
+		f.Flush()
+	}
+}
+
+func (p *responseWriter) Push(target string, opts *http.PushOptions) error {
+	if p, ok := p.ResponseWriter.(http.Pusher); ok {
+		return p.Push(target, opts)
+	}
+	return http.ErrNotSupported
 }
