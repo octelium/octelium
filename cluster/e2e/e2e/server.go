464b14162c6fa7e807e7071a8e0d1b963ddac40c
George B. <george@octelium.com>
2025-12-14 18:30:37 +0000

    cluster/e2e - use xffNumTrustedHops in runGeoIP

--- cluster/e2e/e2e/server.go
+++ cluster/e2e/e2e/server.go
@@ -1762,6 +1762,7 @@ func (s *server) runGeoIP(ctx context.Context) error {
 
 	cc.Spec.Ingress = &corev1.ClusterConfig_Spec_Ingress{
 		UseForwardedForHeader: true,
+		XffNumTrustedHops:     1,
 	}
 
 	const prefixURL = `https://raw.githubusercontent.com/maxmind/MaxMind-DB/refs/heads/main/test-data`
