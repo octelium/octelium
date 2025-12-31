9b38af1f002ed2a8e945b47548eb21fbe9b9eefc
George B. <george@octelium.com>
2025-12-31 18:58:21 +0000

    cluster/authserver - cleanup debug log

--- cluster/authserver/authserver/server.go
+++ cluster/authserver/authserver/server.go
@@ -429,7 +429,7 @@ func (s *server) run(ctx context.Context, grpcMode bool) error {
 		r.HandleFunc("/denied", s.handleDenied).Methods("GET")
 
 		r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			zap.L().Debug("404 req", zap.Any("path", r.URL.Path), zap.String("method", r.Method))
+			// zap.L().Debug("404 req", zap.Any("path", r.URL.Path), zap.String("method", r.Method))
 			if r.Method == "GET" {
 				s.redirectToLogin(w, r)
 			} else {
