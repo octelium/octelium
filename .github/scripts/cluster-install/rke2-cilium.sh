705f7b87cc6d1bf358299055c3f4d97057b9d846
George B. <george@octelium.com>
2026-02-10 10:45:42 +0000

    .github/workflows - introduce rke2-cilium to cluster-install

--- .github/scripts/cluster-install/rke2-cilium.sh
+++ .github/scripts/cluster-install/rke2-cilium.sh
@@ -28,12 +28,6 @@ while [[ $# -gt 0 ]]; do
 done
 
 
-if $IS_UNINSTALL; then
-  /usr/local/bin/k3s-uninstall.sh
-  rm -rf /mnt/octelium/db
-  exit 0
-fi
-
 if [ -z "$DOMAIN" ]; then
   echo "Usage: $0 --domain "example.com"(REQUIRED) --public-ip "1.2.3.4"(OPTIONAL) --version "latest"(OPTIONAL) --nat (OPTIONAL)"
   exit 1
@@ -70,13 +64,36 @@ cp kubectl /usr/local/bin
 chmod 755 /usr/local/bin/kubectl
 
 
-echo -e "\e[1mInstalling k3s\e[0m"
+
+curl -sfL https://get.rke2.io | sh -
+mkdir -p /etc/rancher/rke2/
+cat > /etc/rancher/rke2/config.yaml <<EOF
+cni: none
+disable: 
+- rke2-ingress-nginx
+- rke2-metrics-server
+EOF
+
+systemctl start rke2-server.service
+
+export KUBECONFIG="/etc/rancher/rke2/rke2.yaml"
+
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-${ARCH}.tar.gz
+tar xzf cilium-linux-${ARCH}.tar.gz
+sudo mv cilium /usr/local/bin/
+
+cilium install \
+  --set k8sServiceHost=${EXTERNAL_IP} \
+  --set k8sServicePort=6443 \
+  --set kubeProxyReplacement=true \
+  --set cni.exclusive=false
 
 
-export INSTALL_K3S_EXEC="--disable traefik"
-curl -sfL https://get.k3s.io | sh -
+cilium status --wait
 
-export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+mkdir -p /opt/cni/bin
+wget https://github.com/containernetworking/plugins/releases/download/v1.9.0/cni-plugins-linux-${ARCH}-v1.9.0.tgz
+tar -C /opt/cni/bin -xzf cni-plugins-linux-${ARCH}-v1.9.0.tgz
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
@@ -134,7 +151,7 @@ kubectl create secret generic octelium-redis --from-literal=password=${REDIS_PAS
 echo -e "\e[1mInstalling PostgreSQL, Redis and Multus. This can take a while to finish...\e[0m"
 
 helm install --namespace kube-system octelium-multus oci://registry-1.docker.io/bitnamicharts/multus-cni --version 2.2.7 \
-    --set hostCNIBinDir=/var/lib/rancher/k3s/data/cni/ --set hostCNINetDir=/var/lib/rancher/k3s/agent/etc/cni/net.d \
+    --set hostCNIBinDir=/opt/cni/bin/ --set hostCNINetDir=/etc/cni/net.d \
     --set image.repository=bitnamilegacy/multus-cni --set global.security.allowInsecureImages=true &>/dev/null
 
 helm install octelium-redis oci://registry-1.docker.io/bitnamicharts/redis \
@@ -160,19 +177,13 @@ spec:
   primaryStorage:
     postgresql:
       username: octelium
-      passwordFromSecret:
-        name: octelium-pg
-        namespace: default
-        key: password
+      password: ${PG_PASSWORD}
       host: octelium-pg-postgresql.default.svc
       database: octelium
       port: 5432
   secondaryStorage:
     redis:
-      passwordFromSecret:
-        name: octelium-redis
-        namespace: default
-        key: password
+      password: ${REDIS_PASSWORD}
       host: octelium-redis-master.default.svc
       port: 6379
 $(if [ "$IS_QUIC" = true ]; then
@@ -193,3 +204,6 @@ done
 kubectl get svc -A
 kubectl get ds -A
 kubectl get deployment -A
+
+kubectl port-forward svc/octelium-ingress-dataplane 443:443 -n octelium &
+sleep 3
