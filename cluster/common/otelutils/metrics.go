f1d93ddde5de460982f3545bc6127436474a5b67
George B. <george@octelium.com>
2026-06-08 20:17:13 +0000

    cluster/common - add runtime metrics to SetProcessMetrics

--- cluster/common/otelutils/metrics.go
+++ cluster/common/otelutils/metrics.go
@@ -69,43 +69,170 @@ func GetMeter() metric.Meter {
 	return otel.GetMeterProvider().Meter("default")
 }
 
-/*
-func GetComponentKeyWithPrefix(arg string) string {
-	return fmt.Sprintf("%s.%s.%s", components.MyComponentNamespace(), components.MyComponentType(), arg)
-}
-*/
+const (
+	smHeapAlloc = iota
+	smMemTotal
+	smStacks
+	smGCCycles
+	smGCHeapGoal
+	smCPUGC
+	smCPUTotal
+)
 
 func SetProcessMetrics(ctx context.Context) error {
-
 	meter := GetMeter()
 
-	goroutineNum, err := meter.Int64ObservableUpDownCounter("process.goroutines")
-	if err != nil {
-		return err
-	}
-
-	uptime, err := meter.Int64ObservableCounter("process.uptime", metric.WithUnit("seconds"))
-	if err != nil {
-		return err
-	}
-
-	heapAlloc, err := meter.Int64ObservableUpDownCounter(
-		"process.mem.heap_alloc",
-		metric.WithUnit("bytes"),
-		metric.WithDescription("Bytes allocated by heap"),
+	goroutines, err := meter.Int64ObservableGauge(
+		"process.goroutines",
+		metric.WithUnit("goroutines"),
+		metric.WithDescription("Current number of goroutines"),
 	)
 	if err != nil {
 		return err
 	}
 
-	samples := []metrics.Sample{{Name: "/memory/classes/heap/objects:bytes"}}
-	meter.RegisterCallback(func(_ context.Context, o metric.Observer) error {
-		o.ObserveInt64(goroutineNum, int64(runtime.NumGoroutine()))
-		o.ObserveInt64(uptime, int64(time.Since(components.RuntimeStartedAt()).Seconds()))
-		metrics.Read(samples)
-		o.ObserveInt64(heapAlloc, int64(samples[0].Value.Uint64()))
-		return nil
-	}, goroutineNum, uptime, heapAlloc)
+	gomaxprocs, err := meter.Int64ObservableGauge(
+		"process.gomaxprocs",
+		metric.WithDescription("Current GOMAXPROCS setting"),
+	)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	uptime, err := meter.Int64ObservableCounter(
+		"process.uptime",
+		metric.WithUnit("seconds"),
+		metric.WithDescription("Process uptime in seconds"),
+	)
+	if err != nil {
+		return err
+	}
+
+	heapAlloc, err := meter.Int64ObservableGauge(
+		"process.mem.heap_alloc",
+		metric.WithUnit("bytes"),
+		metric.WithDescription("Bytes of allocated heap objects"),
+	)
+	if err != nil {
+		return err
+	}
+
+	memTotal, err := meter.Int64ObservableGauge(
+		"process.mem.total",
+		metric.WithUnit("bytes"),
+		metric.WithDescription("Total bytes of memory mapped by the Go runtime"),
+	)
+	if err != nil {
+		return err
+	}
+
+	stacks, err := meter.Int64ObservableGauge(
+		"process.mem.stacks",
+		metric.WithUnit("bytes"),
+		metric.WithDescription("Memory used by goroutine stacks"),
+	)
+	if err != nil {
+		return err
+	}
+
+	gcCycles, err := meter.Int64ObservableCounter(
+		"process.gc.cycles",
+		metric.WithUnit("gc-cycles"),
+		metric.WithDescription("Completed GC cycles"),
+	)
+	if err != nil {
+		return err
+	}
+
+	gcHeapGoal, err := meter.Int64ObservableGauge(
+		"process.gc.heap_goal",
+		metric.WithUnit("bytes"),
+		metric.WithDescription("Target heap size for the next GC cycle"),
+	)
+	if err != nil {
+		return err
+	}
+
+	cpuGCSeconds, err := meter.Float64ObservableCounter(
+		"process.cpu.gc_seconds",
+		metric.WithUnit("seconds"),
+		metric.WithDescription("Cumulative CPU time spent in garbage collection"),
+	)
+	if err != nil {
+		return err
+	}
+
+	cpuSeconds, err := meter.Float64ObservableCounter(
+		"process.cpu.seconds",
+		metric.WithUnit("seconds"),
+		metric.WithDescription("Cumulative CPU time consumed by the Go runtime"),
+	)
+	if err != nil {
+		return err
+	}
+
+	samples := []metrics.Sample{
+		smHeapAlloc:  {Name: "/memory/classes/heap/objects:bytes"},
+		smMemTotal:   {Name: "/memory/classes/total:bytes"},
+		smStacks:     {Name: "/memory/classes/heap/stacks:bytes"},
+		smGCCycles:   {Name: "/gc/cycles/total:gc-cycles"},
+		smGCHeapGoal: {Name: "/gc/heap/goal:bytes"},
+		smCPUGC:      {Name: "/cpu/classes/gc/total:cpu-seconds"},
+		smCPUTotal:   {Name: "/cpu/classes/total:cpu-seconds"},
+	}
+
+	_, err = meter.RegisterCallback(func(_ context.Context, o metric.Observer) error {
+		o.ObserveInt64(goroutines, int64(runtime.NumGoroutine()))
+		o.ObserveInt64(gomaxprocs, int64(runtime.GOMAXPROCS(0)))
+		o.ObserveInt64(uptime, int64(time.Since(components.RuntimeStartedAt()).Seconds()))
+
+		metrics.Read(samples)
+
+		observeProcessUint(o, heapAlloc, samples[smHeapAlloc])
+		observeProcessUint(o, memTotal, samples[smMemTotal])
+		observeProcessUint(o, stacks, samples[smStacks])
+		observeProcessUint(o, gcCycles, samples[smGCCycles])
+		observeProcessUint(o, gcHeapGoal, samples[smGCHeapGoal])
+		observeProcessFloat(o, cpuGCSeconds, samples[smCPUGC])
+		observeProcessFloat(o, cpuSeconds, samples[smCPUTotal])
+
+		return nil
+	},
+		goroutines,
+		gomaxprocs,
+		uptime,
+		heapAlloc,
+		memTotal,
+		stacks,
+		gcCycles,
+		gcHeapGoal,
+		cpuGCSeconds,
+		cpuSeconds,
+	)
+
+	return err
+}
+
+func observeProcessUint(o metric.Observer, inst metric.Int64Observable, s metrics.Sample) {
+	if s.Value.Kind() != metrics.KindUint64 {
+		return
+	}
+
+	o.ObserveInt64(inst, uint64ToInt64Saturating(s.Value.Uint64()))
+}
+
+func observeProcessFloat(o metric.Observer, inst metric.Float64Observable, s metrics.Sample) {
+	if s.Value.Kind() != metrics.KindFloat64 {
+		return
+	}
+
+	o.ObserveFloat64(inst, s.Value.Float64())
+}
+
+func uint64ToInt64Saturating(v uint64) int64 {
+	const maxInt64 = uint64(1<<63 - 1)
+	if v > maxInt64 {
+		return int64(maxInt64)
+	}
+	return int64(v)
 }
