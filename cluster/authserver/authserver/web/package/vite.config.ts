de2ec89547955d8bae700f067f4aa3554d1f0343
George B. <george@octelium.com>
2026-06-15 15:21:57 +0000

    cluster/authserver - update @octelium/apis to 1.0.3

--- cluster/authserver/authserver/web/package/vite.config.ts
+++ cluster/authserver/authserver/web/package/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import svgr from "vite-plugin-svgr";
 import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
-import { createRequire } from "module";
 import type { RollupCommonJSOptions } from "@rollup/plugin-commonjs";
+import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 
