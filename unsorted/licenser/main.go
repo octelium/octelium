e77a790541674670400e6bc025921fe5c7e540f2
George B. <george@octelium.com>
2026-07-01 06:29:13 +0000

    unsorted/licenser - improve licenser

--- unsorted/licenser/main.go
+++ unsorted/licenser/main.go
@@ -11,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package main
 
 import (
 	"bytes"
 	"context"
-	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -30,7 +30,6 @@ func main() {
 }
 
 func doMain(ctx context.Context) error {
-
 	clusterComponents := []string{
 		"apiserver",
 		"authserver",
@@ -45,6 +44,7 @@ func doMain(ctx context.Context) error {
 		"portal",
 		"rscserver",
 		"vigil",
+		"wrdpgw",
 	}
 
 	clientComponents := []string{
@@ -63,43 +63,37 @@ func doMain(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	apachev2, err := os.ReadFile("./unsorted/licenser/apachev2.txt")
 	if err != nil {
 		return err
 	}
 
-	if err := os.WriteFile("LICENSE-APACHE", []byte(apachev2), 0666); err != nil {
+	if err := os.WriteFile("LICENSE-APACHE", apachev2, 0666); err != nil {
 		return err
 	}
 
-	if err := os.WriteFile("LICENSE-AGPL-3.0", []byte(agplv3), 0666); err != nil {
+	if err := os.WriteFile("LICENSE-AGPL-3.0", agplv3, 0666); err != nil {
 		return err
 	}
 
 	for _, comp := range clusterComponents {
-
-		if err := os.WriteFile(path.Join("cluster", comp, "LICENSE"), []byte(agplv3), 0666); err != nil {
+		if err := os.WriteFile(path.Join("cluster", comp, "LICENSE"), agplv3, 0666); err != nil {
 			return err
 		}
 	}
 
 	for _, comp := range clientComponents {
-		if err := os.WriteFile(path.Join("client", comp, "LICENSE"), []byte(apachev2), 0666); err != nil {
+		if err := os.WriteFile(path.Join("client", comp, "LICENSE"), apachev2, 0666); err != nil {
 			return err
 		}
 	}
 
 	for _, mod := range additionalApacheModules {
-		if err := os.WriteFile(path.Join(mod, "LICENSE"), []byte(apachev2), 0666); err != nil {
+		if err := os.WriteFile(path.Join(mod, "LICENSE"), apachev2, 0666); err != nil {
 			return err
 		}
 	}
-
-	/*
-		if err := setClusterHeader(ctx); err != nil {
-			return err
-		}
-	*/
 
 	if err := setHeader(ctx, "./apis", apacheHeader); err != nil {
 		return err
@@ -133,88 +127,191 @@ func setClientHeader(ctx context.Context) error {
 }
 
 func setHeader(ctx context.Context, rootPath string, header string) error {
+	_ = ctx
 
-	if err := filepath.Walk(rootPath,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
+	return filepath.Walk(rootPath, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			if shouldSkipDir(info.Name()) {
+				return filepath.SkipDir
 			}
-
-			if info.IsDir() {
-				return nil
-			}
-
-			if !info.Mode().IsRegular() {
-				return nil
-			}
-
-			if !strings.HasSuffix(path, ".go") {
-				return nil
-			}
-
-			cn, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
-
-			/*
-				if strings.HasPrefix(string(cn), header) {
-					return nil
-				}
-			*/
-
-			pkgIdx := getIdx(cn[:])
-			if pkgIdx < 0 {
-				return nil
-			}
-
-			newFile := header + "\n" + string(cn[pkgIdx:])
-
-			// fmt.Printf("%s", newFile)
-
-			if err := os.WriteFile(path, []byte(newFile), info.Mode().Perm()); err != nil {
-				return err
-			}
-
-			/*
-				nd := string(cn)
-
-				nd = strings.TrimSpace(nd)
-
-				for _, hdr := range oldHeaders {
-					if strings.Contains(string(nd), hdr) {
-						nd = strings.Replace(string(nd), hdr, "", 1)
-					}
-				}
-
-				nd = header + "\n" + nd + "\n"
-
-				fmt.Printf("Applying to %s\n", path)
-
-				if err := os.WriteFile(path, []byte(nd), info.Mode().Perm()); err != nil {
-					return err
-				}
-			*/
-
 			return nil
-		}); err != nil {
-		return err
+		}
+
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		if !strings.HasSuffix(filePath, ".go") {
+			return nil
+		}
+
+		cn, err := os.ReadFile(filePath)
+		if err != nil {
+			return err
+		}
+
+		newFile, ok := applyHeader(string(cn), header)
+		if !ok {
+			return nil
+		}
+
+		if newFile == string(cn) {
+			return nil
+		}
+
+		return os.WriteFile(filePath, []byte(newFile), info.Mode().Perm())
+	})
+}
+
+func shouldSkipDir(name string) bool {
+	switch name {
+	case ".git", ".idea", ".vscode", "node_modules", "vendor", "dist", "build", "target", ".libs":
+		return true
+	default:
+		return false
 	}
-	return nil
+}
+
+func applyHeader(src string, header string) (string, bool) {
+	src = strings.ReplaceAll(src, "\r\n", "\n")
+
+	pkgIdx := packageIdx(src)
+	if pkgIdx < 0 {
+		return "", false
+	}
+
+	prefix := src[:pkgIdx]
+	body := src[pkgIdx:]
+
+	buildLines, remainingPrefix := extractBuildConstraints(prefix)
+	remainingPrefix = stripKnownLicenseHeaders(remainingPrefix)
+	remainingPrefix = strings.TrimLeft(remainingPrefix, "\n")
+
+	var b strings.Builder
+
+	if len(buildLines) > 0 {
+		b.WriteString(strings.Join(buildLines, "\n"))
+		b.WriteString("\n\n")
+	}
+
+	b.WriteString(strings.TrimRight(header, "\n"))
+	b.WriteString("\n\n")
+
+	if remainingPrefix != "" {
+		b.WriteString(remainingPrefix)
+		if !strings.HasSuffix(remainingPrefix, "\n") {
+			b.WriteString("\n")
+		}
+	}
+
+	b.WriteString(body)
+
+	return b.String(), true
+}
+
+func packageIdx(src string) int {
+	offset := 0
+
+	for _, line := range splitLinesKeepingEnd(src) {
+		trimmed := strings.TrimLeft(line, " \t")
+		if strings.HasPrefix(trimmed, "package ") {
+			return offset + len(line) - len(trimmed)
+		}
+		offset += len(line)
+	}
+
+	return -1
+}
+
+func extractBuildConstraints(prefix string) ([]string, string) {
+	var goBuildLines []string
+	var plusBuildLines []string
+	var remaining []string
+
+	for _, line := range splitLinesKeepingEnd(prefix) {
+		lineNoEnd := strings.TrimRight(line, "\n")
+		trimmed := strings.TrimSpace(lineNoEnd)
+
+		switch {
+		case strings.HasPrefix(trimmed, "//go:build"):
+			goBuildLines = append(goBuildLines, lineNoEnd)
+		case strings.HasPrefix(trimmed, "// +build"):
+			plusBuildLines = append(plusBuildLines, lineNoEnd)
+		default:
+			remaining = append(remaining, line)
+		}
+	}
+
+	buildLines := make([]string, 0, len(goBuildLines)+len(plusBuildLines))
+	buildLines = append(buildLines, goBuildLines...)
+	buildLines = append(buildLines, plusBuildLines...)
+
+	return buildLines, strings.Join(remaining, "")
+}
+
+func stripKnownLicenseHeaders(s string) string {
+	headers := []string{
+		apacheHeader,
+		agplHeader,
+	}
+
+	for {
+		before := s
+		s = strings.TrimLeft(s, " \t\n")
+
+		matched := false
+		for _, hdr := range headers {
+			h := strings.TrimRight(strings.ReplaceAll(hdr, "\r\n", "\n"), "\n")
+			if strings.HasPrefix(s, h) {
+				s = s[len(h):]
+				matched = true
+				break
+			}
+		}
+
+		if !matched || s == before {
+			break
+		}
+	}
+
+	return strings.TrimLeft(s, " \t\n")
+}
+
+func splitLinesKeepingEnd(s string) []string {
+	if s == "" {
+		return nil
+	}
+
+	var ret []string
+	for len(s) > 0 {
+		idx := strings.IndexByte(s, '\n')
+		if idx < 0 {
+			ret = append(ret, s)
+			break
+		}
+
+		ret = append(ret, s[:idx+1])
+		s = s[idx+1:]
+	}
+
+	return ret
 }
 
 func getIdx(src []byte) int {
-
 	ret := bytes.Index(src, []byte("package "))
-	if idx := bytes.Index(src, []byte("//go:build")); idx > 0 && idx < ret {
+	if idx := bytes.Index(src, []byte("//go:build")); idx >= 0 && idx < ret {
 		ret = idx
 	}
 
-	if idx := bytes.Index(src, []byte("// +build")); idx > 0 && idx < ret {
+	if idx := bytes.Index(src, []byte("// +build")); idx >= 0 && idx < ret {
 		ret = idx
 	}
 
-	if idx := bytes.Index(src, []byte("// Code generated")); idx > 0 && idx < ret {
+	if idx := bytes.Index(src, []byte("// Code generated")); idx >= 0 && idx < ret {
 		ret = idx
 	}
 
