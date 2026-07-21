574c4bc77a91550f6d665155e8d23ece75590564
George B. <george@octelium.com>
2026-07-21 05:18:29 +0000

    cluster/apiserver - add more Credential tests

--- cluster/apiserver/apiserver/admin/credential_test.go
+++ cluster/apiserver/apiserver/admin/credential_test.go
@@ -19,16 +19,38 @@ package admin
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/octelium/octelium/apis/main/corev1"
 	"github.com/octelium/octelium/apis/main/metav1"
 	"github.com/octelium/octelium/cluster/common/jwkctl"
 	"github.com/octelium/octelium/cluster/common/tests"
 	"github.com/octelium/octelium/pkg/apiutils/umetav1"
+	"github.com/octelium/octelium/pkg/common/pbutils"
 	"github.com/octelium/octelium/pkg/grpcerr"
 	"github.com/octelium/octelium/pkg/utils/utilrand"
 	"github.com/stretchr/testify/assert"
 )
+
+func tstGenCredentialUser(ctx context.Context, t *testing.T, srv *Server, typ corev1.User_Spec_Type) *corev1.User {
+	usr := tests.GenUser(nil)
+	usr.Spec.Type = typ
+	usr, err := srv.CreateUser(ctx, usr)
+	assert.Nil(t, err, "%+v", err)
+	return usr
+}
+
+func tstGenCredential(user string, typ corev1.Credential_Spec_Type) *corev1.Credential {
+	return &corev1.Credential{
+		Metadata: &metav1.Metadata{
+			Name: utilrand.GetRandomStringCanonical(8),
+		},
+		Spec: &corev1.Credential_Spec{
+			User: user,
+			Type: typ,
+		},
+	}
+}
 
 func TestCredential(t *testing.T) {
 	ctx := context.Background()
@@ -51,16 +73,16 @@ func TestCredential(t *testing.T) {
 				Name: utilrand.GetRandomStringCanonical(8),
 			},
 			Spec: &corev1.Credential_Spec{
+				Type: corev1.Credential_Spec_AUTH_TOKEN,
 				User: "does-not-exist",
 			},
 		})
 		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
 	}
 
 	{
-		usr := tests.GenUser(nil)
-		usr, err = srv.CreateUser(ctx, usr)
-		assert.Nil(t, err)
+		usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_HUMAN)
 		cred, err := srv.CreateCredential(ctx, &corev1.Credential{
 			Metadata: &metav1.Metadata{
 				Name: utilrand.GetRandomStringCanonical(8),
@@ -71,7 +93,7 @@ func TestCredential(t *testing.T) {
 				SessionType: corev1.Session_Status_CLIENT,
 			},
 		})
-		assert.Nil(t, err)
+		assert.Nil(t, err, "%+v", err)
 
 		tknResp, err := srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{
 			CredentialRef: umetav1.GetObjectReference(cred),
@@ -89,16 +111,24 @@ func TestCredential(t *testing.T) {
 		assert.Equal(t, tkn.Status.UserRef.Uid, usr.Metadata.Uid)
 		assert.Equal(t, tkn.Status.TokenID, claims.TokenID)
 		assert.Equal(t, corev1.Credential_Spec_AUTH_TOKEN, tkn.Spec.Type)
+		assert.Equal(t, uint32(1), tkn.Status.TotalRotations)
+
+		_, err = srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{
+			CredentialRef: umetav1.GetObjectReference(cred),
+		})
+		assert.Nil(t, err, "%+v", err)
+
+		tkn, err = srv.GetCredential(ctx, &metav1.GetOptions{Uid: cred.Metadata.Uid})
+		assert.Nil(t, err)
+		assert.Equal(t, uint32(2), tkn.Status.TotalRotations)
+		assert.NotEqual(t, claims.TokenID, tkn.Status.TokenID)
 
 		_, err = srv.DeleteCredential(ctx, &metav1.DeleteOptions{Uid: claims.UID})
 		assert.Nil(t, err)
-
 	}
 
 	{
-		usr := tests.GenUser(nil)
-		_, err = srv.CreateUser(ctx, usr)
-		assert.Nil(t, err)
+		usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_HUMAN)
 		_, err := srv.CreateCredential(ctx, &corev1.Credential{
 			Metadata: &metav1.Metadata{
 				Name: utilrand.GetRandomStringCanonical(8),
@@ -109,12 +139,11 @@ func TestCredential(t *testing.T) {
 			},
 		})
 		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
 	}
 
 	{
-		usr := tests.GenUser(nil)
-		_, err = srv.CreateUser(ctx, usr)
-		assert.Nil(t, err)
+		usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
 		cred, err := srv.CreateCredential(ctx, &corev1.Credential{
 			Metadata: &metav1.Metadata{
 				Name: utilrand.GetRandomStringCanonical(8),
@@ -125,7 +154,7 @@ func TestCredential(t *testing.T) {
 				SessionType: corev1.Session_Status_CLIENT,
 			},
 		})
-		assert.Nil(t, err)
+		assert.Nil(t, err, "%+v", err)
 
 		tknResp, err := srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{
 			CredentialRef: umetav1.GetObjectReference(cred),
@@ -147,13 +176,10 @@ func TestCredential(t *testing.T) {
 
 		_, err = srv.DeleteCredential(ctx, &metav1.DeleteOptions{Uid: claims.UID})
 		assert.Nil(t, err)
-
 	}
 
 	{
-		usr := tests.GenUser(nil)
-		_, err = srv.CreateUser(ctx, usr)
-		assert.Nil(t, err)
+		usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
 		cred, err := srv.CreateCredential(ctx, &corev1.Credential{
 			Metadata: &metav1.Metadata{
 				Name: utilrand.GetRandomStringCanonical(8),
@@ -164,7 +190,7 @@ func TestCredential(t *testing.T) {
 				SessionType: corev1.Session_Status_CLIENTLESS,
 			},
 		})
-		assert.Nil(t, err)
+		assert.Nil(t, err, "%+v", err)
 
 		{
 			tknResp, err := srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{
@@ -213,5 +239,391 @@ func TestCredential(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.True(t, grpcerr.IsNotFound(err))
 	}
+}
 
+func TestValidateCredential(t *testing.T) {
+	ctx := context.Background()
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	srv := newFakeServer(tst.C)
+
+	human := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_HUMAN)
+	workload := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
+
+	policy, err := srv.CreatePolicy(ctx, &corev1.Policy{
+		Metadata: &metav1.Metadata{
+			Name: utilrand.GetRandomStringCanonical(8),
+		},
+		Spec: &corev1.Policy_Spec{
+			Rules: []*corev1.Policy_Spec_Rule{
+				{
+					Condition: &corev1.Condition{
+						Type: &corev1.Condition_MatchAny{
+							MatchAny: true,
+						},
+					},
+					Effect: corev1.Policy_Spec_Rule_ALLOW,
+				},
+			},
+		},
+	})
+	assert.Nil(t, err, "%+v", err)
+
+	invalids := []*corev1.Credential{
+		{},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+		},
+		{
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+				Type: corev1.Credential_Spec_AUTH_TOKEN,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+				Type: corev1.Credential_Spec_Type(1000),
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:        human.Metadata.Name,
+				Type:        corev1.Credential_Spec_AUTH_TOKEN,
+				SessionType: corev1.Session_Status_Type(1000),
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				Type: corev1.Credential_Spec_AUTH_TOKEN,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: utilrand.GetRandomStringCanonical(8),
+				Type: corev1.Credential_Spec_AUTH_TOKEN,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+				Type: corev1.Credential_Spec_OAUTH2,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+				Type: corev1.Credential_Spec_ACCESS_TOKEN,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:      human.Metadata.Name,
+				Type:      corev1.Credential_Spec_AUTH_TOKEN,
+				ExpiresAt: pbutils.Timestamp(time.Now().Add(-time.Hour)),
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:      human.Metadata.Name,
+				Type:      corev1.Credential_Spec_AUTH_TOKEN,
+				ExpiresAt: pbutils.Timestamp(time.Now().Add(credMaxExpiryDuration + time.Hour*24)),
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:               human.Metadata.Name,
+				Type:               corev1.Credential_Spec_AUTH_TOKEN,
+				MaxAuthentications: credMaxAuthentications + 1,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:       human.Metadata.Name,
+				Type:       corev1.Credential_Spec_AUTH_TOKEN,
+				AutoDelete: true,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+				Type: corev1.Credential_Spec_AUTH_TOKEN,
+				Authorization: &corev1.Credential_Spec_Authorization{
+					Policies: []string{utilrand.GetRandomStringCanonical(8)},
+				},
+			},
+		},
+	}
+
+	for _, invalid := range invalids {
+		_, err = srv.CreateCredential(ctx, invalid)
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
+	}
+
+	valids := []*corev1.Credential{
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: human.Metadata.Name,
+				Type: corev1.Credential_Spec_AUTH_TOKEN,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:        human.Metadata.Name,
+				Type:        corev1.Credential_Spec_AUTH_TOKEN,
+				SessionType: corev1.Session_Status_CLIENTLESS,
+				ExpiresAt:   pbutils.Timestamp(time.Now().Add(time.Hour)),
+				IsDisabled:  true,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User:               human.Metadata.Name,
+				Type:               corev1.Credential_Spec_AUTH_TOKEN,
+				MaxAuthentications: 10,
+				AutoDelete:         true,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: workload.Metadata.Name,
+				Type: corev1.Credential_Spec_OAUTH2,
+			},
+		},
+		{
+			Metadata: &metav1.Metadata{Name: utilrand.GetRandomStringCanonical(8)},
+			Spec: &corev1.Credential_Spec{
+				User: workload.Metadata.Name,
+				Type: corev1.Credential_Spec_ACCESS_TOKEN,
+				Authorization: &corev1.Credential_Spec_Authorization{
+					Policies: []string{policy.Metadata.Name},
+					InlinePolicies: []*corev1.InlinePolicy{
+						{
+							Name: utilrand.GetRandomStringCanonical(8),
+							Spec: &corev1.Policy_Spec{
+								Rules: []*corev1.Policy_Spec_Rule{
+									{
+										Condition: &corev1.Condition{
+											Type: &corev1.Condition_MatchAny{
+												MatchAny: true,
+											},
+										},
+										Effect: corev1.Policy_Spec_Rule_ALLOW,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, valid := range valids {
+		item, err := srv.CreateCredential(ctx, valid)
+		assert.Nil(t, err, "%+v", err)
+		assert.NotNil(t, item.Status.UserRef)
+		assert.NotEmpty(t, item.Status.Id)
+
+		_, err = srv.CreateCredential(ctx, valid)
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.AlreadyExists(err), "%+v", err)
+	}
+}
+
+func TestUpdateCredential(t *testing.T) {
+	ctx := context.Background()
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	srv := newFakeServer(tst.C)
+
+	usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
+	usr2 := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
+
+	cred, err := srv.CreateCredential(ctx, tstGenCredential(usr.Metadata.Name, corev1.Credential_Spec_AUTH_TOKEN))
+	assert.Nil(t, err, "%+v", err)
+
+	{
+		cred.Spec.MaxAuthentications = 42
+		cred.Spec.IsDisabled = true
+		updated, err := srv.UpdateCredential(ctx, cred)
+		assert.Nil(t, err, "%+v", err)
+		assert.Equal(t, uint32(42), updated.Spec.MaxAuthentications)
+		assert.True(t, updated.Spec.IsDisabled)
+		assert.Equal(t, usr.Metadata.Uid, updated.Status.UserRef.Uid)
+		cred = updated
+	}
+
+	{
+		item := tstCloneCredential(cred)
+		item.Spec.Type = corev1.Credential_Spec_OAUTH2
+		_, err = srv.UpdateCredential(ctx, item)
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
+	}
+
+	{
+		item := tstCloneCredential(cred)
+		item.Spec.User = usr2.Metadata.Name
+		_, err = srv.UpdateCredential(ctx, item)
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
+	}
+
+	{
+		item := tstCloneCredential(cred)
+		item.Spec.Type = corev1.Credential_Spec_TYPE_UNKNOWN
+		_, err = srv.UpdateCredential(ctx, item)
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
+	}
+
+	{
+		item := tstGenCredential(usr.Metadata.Name, corev1.Credential_Spec_AUTH_TOKEN)
+		_, err = srv.UpdateCredential(ctx, item)
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsNotFound(err), "%+v", err)
+	}
+}
+
+func TestListCredential(t *testing.T) {
+	ctx := context.Background()
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	srv := newFakeServer(tst.C)
+
+	usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
+	usr2 := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
+
+	for i := 0; i < 3; i++ {
+		_, err = srv.CreateCredential(ctx, tstGenCredential(usr.Metadata.Name, corev1.Credential_Spec_AUTH_TOKEN))
+		assert.Nil(t, err, "%+v", err)
+	}
+
+	_, err = srv.CreateCredential(ctx, tstGenCredential(usr2.Metadata.Name, corev1.Credential_Spec_AUTH_TOKEN))
+	assert.Nil(t, err, "%+v", err)
+
+	{
+		itemList, err := srv.ListCredential(ctx, &corev1.ListCredentialOptions{})
+		assert.Nil(t, err, "%+v", err)
+		assert.True(t, len(itemList.Items) >= 4)
+	}
+
+	{
+		itemList, err := srv.ListCredential(ctx, &corev1.ListCredentialOptions{
+			UserRef: umetav1.GetObjectReference(usr),
+		})
+		assert.Nil(t, err, "%+v", err)
+		assert.Equal(t, 3, len(itemList.Items))
+		for _, item := range itemList.Items {
+			assert.Equal(t, usr.Metadata.Uid, item.Status.UserRef.Uid)
+		}
+	}
+
+	{
+		_, err := srv.ListCredential(ctx, &corev1.ListCredentialOptions{
+			UserRef: &metav1.ObjectReference{
+				Uid: utilrand.GetRandomStringCanonical(8),
+			},
+		})
+		assert.NotNil(t, err)
+	}
+}
+
+func TestGenerateCredentialToken(t *testing.T) {
+	ctx := context.Background()
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	srv := newFakeServer(tst.C)
+
+	usr := tstGenCredentialUser(ctx, t, srv, corev1.User_Spec_WORKLOAD)
+
+	{
+		_, err = srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{})
+		assert.NotNil(t, err)
+	}
+
+	{
+		_, err = srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{
+			CredentialRef: &metav1.ObjectReference{
+				Uid: utilrand.GetRandomStringCanonical(8),
+			},
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		cred, err := srv.CreateCredential(ctx, &corev1.Credential{
+			Metadata: &metav1.Metadata{
+				Name: utilrand.GetRandomStringCanonical(8),
+			},
+			Spec: &corev1.Credential_Spec{
+				User:       usr.Metadata.Name,
+				Type:       corev1.Credential_Spec_AUTH_TOKEN,
+				IsDisabled: true,
+			},
+		})
+		assert.Nil(t, err, "%+v", err)
+
+		_, err = srv.GenerateCredentialToken(ctx, &corev1.GenerateCredentialTokenRequest{
+			CredentialRef: umetav1.GetObjectReference(cred),
+		})
+		assert.NotNil(t, err)
+		assert.True(t, grpcerr.IsInvalidArg(err), "%+v", err)
+	}
+}
+
+func tstCloneCredential(arg *corev1.Credential) *corev1.Credential {
+	return &corev1.Credential{
+		Metadata: &metav1.Metadata{
+			Name: arg.Metadata.Name,
+			Uid:  arg.Metadata.Uid,
+		},
+		Spec: &corev1.Credential_Spec{
+			User:               arg.Spec.User,
+			Type:               arg.Spec.Type,
+			MaxAuthentications: arg.Spec.MaxAuthentications,
+			SessionType:        arg.Spec.SessionType,
+			IsDisabled:         arg.Spec.IsDisabled,
+			AutoDelete:         arg.Spec.AutoDelete,
+		},
+	}
 }
