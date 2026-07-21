28edbfc8b185c83f5efc712152d42467c6385999
George B. <george@octelium.com>
2026-07-21 09:26:09 +0000

    cluster/authserver - add more tests for provider utils

--- cluster/authserver/authserver/providers/utils/utils_test.go
+++ cluster/authserver/authserver/providers/utils/utils_test.go
@@ -21,11 +21,15 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/octelium/octelium/apis/main/corev1"
+	"github.com/octelium/octelium/apis/main/metav1"
+	"github.com/octelium/octelium/apis/rsc/rmetav1"
+	"github.com/octelium/octelium/cluster/apiserver/apiserver/admin"
 	"github.com/octelium/octelium/cluster/common/celengine"
 	"github.com/octelium/octelium/cluster/common/tests"
 	"github.com/octelium/octelium/pkg/utils/utilrand"
@@ -221,5 +225,369 @@ func TestIsAssertionIssuerForIdentityProvider(t *testing.T) {
 				},
 			}, tknStr))
 		}
+	}
+}
+
+type testRegisteredClaims struct {
+	jwt.RegisteredClaims
+}
+
+func newTestAssertion(t *testing.T, issuer string) string {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	assert.Nil(t, err)
+
+	tkn := jwt.NewWithClaims(jwt.SigningMethodRS256, &testRegisteredClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   utilrand.GetRandomStringCanonical(8),
+			Issuer:    issuer,
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+		},
+	})
+
+	ret, err := tkn.SignedString(priv)
+	assert.Nil(t, err)
+
+	return ret
+}
+
+func TestGetCallbackURL(t *testing.T) {
+	assert.Equal(t, "https://example.com/callback", GetCallbackURL("example.com"))
+	assert.Equal(t, "https://sub.example.com/callback", GetCallbackURL("sub.example.com"))
+	assert.NotEqual(t, GetCallbackURL("a.com"), GetCallbackURL("b.com"))
+}
+
+func TestPeekAssertionIssuerEdgeCases(t *testing.T) {
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+
+	{
+		_, err := peekAssertionIssuer(strings.Repeat("a", 15001))
+		assert.NotNil(t, err)
+	}
+
+	{
+		_, err := peekAssertionIssuer("aaa.bbb.ccc")
+		assert.NotNil(t, err)
+	}
+
+	{
+		_, err := peekAssertionIssuer("not-a-jwt-at-all")
+		assert.NotNil(t, err)
+	}
+
+	{
+		priv, err := rsa.GenerateKey(rand.Reader, 2048)
+		assert.Nil(t, err)
+
+		tkn := jwt.NewWithClaims(jwt.SigningMethodRS256, &testRegisteredClaims{
+			RegisteredClaims: jwt.RegisteredClaims{
+				Subject:   utilrand.GetRandomStringCanonical(8),
+				IssuedAt:  jwt.NewNumericDate(time.Now()),
+				ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+			},
+		})
+
+		tknStr, err := tkn.SignedString(priv)
+		assert.Nil(t, err)
+
+		ret, err := peekAssertionIssuer(tknStr)
+		assert.Nil(t, err)
+		assert.Equal(t, "", ret)
+	}
+
+	{
+		issuer := fmt.Sprintf("https://%s.example.com", utilrand.GetRandomStringCanonical(8))
+
+		ret, err := peekAssertionIssuer(newTestAssertion(t, issuer))
+		assert.Nil(t, err)
+		assert.Equal(t, issuer, ret)
+	}
+}
+
+func TestIsAssertionIssuerForIdentityProviderNegative(t *testing.T) {
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+
+	issuer := fmt.Sprintf("https://%s.example.com", utilrand.GetRandomStringCanonical(8))
+	assertion := newTestAssertion(t, issuer)
+
+	newIDP := func(status corev1.IdentityProvider_Status_Type,
+		spec *corev1.IdentityProvider_Spec) *corev1.IdentityProvider {
+		return &corev1.IdentityProvider{
+			Metadata: &metav1.Metadata{
+				Name: utilrand.GetRandomStringCanonical(8),
+			},
+			Spec: spec,
+			Status: &corev1.IdentityProvider_Status{
+				Type: status,
+			},
+		}
+	}
+
+	oidcSpec := func(issuerURL string) *corev1.IdentityProvider_Spec {
+		return &corev1.IdentityProvider_Spec{
+			Type: &corev1.IdentityProvider_Spec_OidcIdentityToken{
+				OidcIdentityToken: &corev1.IdentityProvider_Spec_OIDCIdentityToken{
+					Type: &corev1.IdentityProvider_Spec_OIDCIdentityToken_IssuerURL{
+						IssuerURL: issuerURL,
+					},
+				},
+			},
+		}
+	}
+
+	{
+		assert.False(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC, oidcSpec(issuer)), assertion))
+	}
+
+	{
+		assert.False(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_GITHUB, oidcSpec(issuer)), assertion))
+	}
+
+	{
+		assert.False(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC_IDENTITY_TOKEN,
+				&corev1.IdentityProvider_Spec{}), assertion))
+	}
+
+	{
+		assert.False(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC_IDENTITY_TOKEN,
+				oidcSpec("https://other.example.com")), assertion))
+	}
+
+	{
+		assert.False(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC_IDENTITY_TOKEN,
+				oidcSpec(issuer)), "not-a-jwt"))
+	}
+
+	{
+		assert.False(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC_IDENTITY_TOKEN,
+				oidcSpec(issuer)), ""))
+	}
+
+	{
+		assert.True(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC_IDENTITY_TOKEN,
+				&corev1.IdentityProvider_Spec{
+					Type: &corev1.IdentityProvider_Spec_OidcIdentityToken{
+						OidcIdentityToken: &corev1.IdentityProvider_Spec_OIDCIdentityToken{
+							Issuer: issuer,
+						},
+					},
+				}), assertion))
+	}
+
+	{
+		assert.True(t, IsAssertionIssuerForIdentityProvider(
+			newIDP(corev1.IdentityProvider_Status_OIDC_IDENTITY_TOKEN,
+				oidcSpec(issuer+"/")), assertion))
+	}
+}
+
+func TestGetUserFromIdentifier(t *testing.T) {
+
+	ctx := context.Background()
+
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	fakeC := tst.C
+
+	adminSrv := admin.NewServer(&admin.Opts{
+		OcteliumC:  fakeC.OcteliumC,
+		IsEmbedded: true,
+	})
+
+	sec, err := fakeC.OcteliumC.CoreC().CreateSecret(ctx, &corev1.Secret{
+		Metadata: &metav1.Metadata{
+			Name: utilrand.GetRandomStringCanonical(8),
+		},
+		Spec: &corev1.Secret_Spec{},
+		Data: &corev1.Secret_Data{
+			Type: &corev1.Secret_Data_Value{
+				Value: utilrand.GetRandomString(32),
+			},
+		},
+	})
+	assert.Nil(t, err)
+
+	idp, err := fakeC.OcteliumC.CoreC().CreateIdentityProvider(ctx, &corev1.IdentityProvider{
+		Metadata: &metav1.Metadata{
+			Name:        "github-1",
+			DisplayName: "Github 1",
+		},
+
+		Spec: &corev1.IdentityProvider_Spec{
+			Type: &corev1.IdentityProvider_Spec_Github_{
+				Github: &corev1.IdentityProvider_Spec_Github{
+					ClientID: "xxx",
+					ClientSecret: &corev1.IdentityProvider_Spec_Github_ClientSecret{
+						Type: &corev1.IdentityProvider_Spec_Github_ClientSecret_FromSecret{
+							FromSecret: sec.Metadata.Name,
+						},
+					},
+				},
+			},
+		},
+		Status: &corev1.IdentityProvider_Status{
+			Type: corev1.IdentityProvider_Status_GITHUB,
+		},
+	})
+	assert.Nil(t, err)
+
+	idpName := idp.Metadata.Name
+
+	newUsr := func(identifier string, usrType corev1.User_Spec_Type) *corev1.User {
+		usr := &corev1.User{
+			Metadata: &metav1.Metadata{
+				Name: fmt.Sprintf("usr-%s", utilrand.GetRandomStringLowercase(8)),
+			},
+			Spec: &corev1.User_Spec{
+				Type: usrType,
+			},
+		}
+
+		if identifier != "" {
+			usr.Spec.Authentication = &corev1.User_Spec_Authentication{
+				Identities: []*corev1.User_Spec_Authentication_Identity{
+					{
+						IdentityProvider: idpName,
+						Identifier:       identifier,
+					},
+				},
+			}
+		}
+
+		ret, err := adminSrv.CreateUser(ctx, usr)
+		assert.Nil(t, err, "%+v", err)
+		return ret
+	}
+
+	{
+		_, err := GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           "",
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		_, err := GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           utilrand.GetRandomStringCanonical(12),
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		identifier := utilrand.GetRandomStringLowercase(10)
+		usr := newUsr(identifier, corev1.User_Spec_WORKLOAD)
+
+		ret, err := GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           identifier,
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.Nil(t, err, "%+v", err)
+		assert.Equal(t, usr.Metadata.Uid, ret.Metadata.Uid)
+	}
+
+	{
+		identifier := utilrand.GetRandomStringLowercase(10)
+		newUsr(identifier, corev1.User_Spec_WORKLOAD)
+
+		_, err := GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           identifier,
+			UserType:             corev1.User_Spec_HUMAN,
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		identifier := utilrand.GetRandomStringLowercase(10)
+		usr := newUsr(identifier, corev1.User_Spec_WORKLOAD)
+
+		usr.Spec.IsDisabled = true
+		_, err := adminSrv.UpdateUser(ctx, usr)
+		assert.Nil(t, err)
+
+		_, err = GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           identifier,
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		identifier := utilrand.GetRandomStringLowercase(10)
+		usr := newUsr(identifier, corev1.User_Spec_WORKLOAD)
+
+		stored, err := fakeC.OcteliumC.CoreC().GetUser(ctx, &rmetav1.GetOptions{
+			Uid: usr.Metadata.Uid,
+		})
+		assert.Nil(t, err)
+
+		stored.Status.IsLocked = true
+		_, err = fakeC.OcteliumC.CoreC().UpdateUser(ctx, stored)
+		assert.Nil(t, err)
+
+		_, err = GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           identifier,
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		identifier := utilrand.GetRandomStringLowercase(10)
+		newUsr(identifier, corev1.User_Spec_WORKLOAD)
+
+		_, err := GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: utilrand.GetRandomStringCanonical(8),
+			Identifier:           identifier,
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		identifier := utilrand.GetRandomStringLowercase(10)
+		newUsr(identifier, corev1.User_Spec_WORKLOAD)
+
+		_, err := GetUserFromIdentifier(ctx, &GetUserFromIdentifierOpts{
+			OcteliumC:            fakeC.OcteliumC,
+			IdentityProviderName: idpName,
+			Identifier:           strings.ToUpper(identifier),
+			UserType:             corev1.User_Spec_WORKLOAD,
+		})
+		assert.NotNil(t, err)
 	}
 }
