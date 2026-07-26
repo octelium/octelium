654808d11a5f71ef4c06b84010efeaac25cbc02c
George B. <george@octelium.com>
2026-07-26 02:53:00 +0000

    cluster/apiserver - add TestListNamespace in user

--- cluster/apiserver/apiserver/user/namespace_test.go
+++ cluster/apiserver/apiserver/user/namespace_test.go
@@ -17,9 +17,94 @@
 package user
 
 import (
+	"context"
 	"testing"
+
+	"github.com/octelium/octelium/apis/main/corev1"
+	"github.com/octelium/octelium/apis/main/metav1"
+	"github.com/octelium/octelium/apis/main/userv1"
+	"github.com/octelium/octelium/cluster/common/tests"
+	"github.com/octelium/octelium/cluster/common/tests/tstuser"
+	"github.com/octelium/octelium/pkg/utils/utilrand"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestListNetwork(t *testing.T) {
+func TestListNamespace(t *testing.T) {
+	ctx := context.Background()
 
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	usrSrv, adminSrv := newFakeServers(tst.C)
+
+	usrT, err := tstuser.NewUser(tst.C.OcteliumC, adminSrv, usrSrv, nil)
+	assert.Nil(t, err, "%+v", err)
+
+	{
+		itemList, err := usrSrv.ListNamespace(usrT.Ctx(), &userv1.ListNamespaceOptions{})
+		assert.Nil(t, err, "%+v", err)
+		assert.Equal(t, "user/v1", itemList.ApiVersion)
+		assert.Equal(t, "NamespaceList", itemList.Kind)
+		assert.NotNil(t, itemList.ListResponseMeta)
+	}
+
+	ns, err := adminSrv.CreateNamespace(ctx, &corev1.Namespace{
+		Metadata: &metav1.Metadata{
+			Name:        utilrand.GetRandomStringCanonical(8),
+			DisplayName: "My Namespace",
+			Description: "My description",
+			Labels: map[string]string{
+				"key": "val",
+			},
+		},
+		Spec: &corev1.Namespace_Spec{},
+	})
+	assert.Nil(t, err, "%+v", err)
+
+	{
+		itemList, err := usrSrv.ListNamespace(usrT.Ctx(), &userv1.ListNamespaceOptions{})
+		assert.Nil(t, err, "%+v", err)
+
+		var found *userv1.Namespace
+		for _, itm := range itemList.Items {
+			assert.NotNil(t, itm.Spec)
+			assert.NotNil(t, itm.Status)
+			assert.NotEmpty(t, itm.Metadata.Uid)
+			assert.NotEmpty(t, itm.Metadata.Name)
+
+			if itm.Metadata.Uid == ns.Metadata.Uid {
+				found = itm
+			}
+		}
+
+		assert.NotNil(t, found)
+		assert.Equal(t, ns.Metadata.Name, found.Metadata.Name)
+		assert.Equal(t, "My Namespace", found.Metadata.DisplayName)
+		assert.Equal(t, "My description", found.Metadata.Description)
+		assert.Nil(t, found.Metadata.Labels)
+	}
+
+	{
+		nsList, err := tst.C.OcteliumC.CoreC().ListNamespace(ctx, nil)
+		assert.Nil(t, err, "%+v", err)
+
+		itemList, err := usrSrv.ListNamespace(usrT.Ctx(), &userv1.ListNamespaceOptions{})
+		assert.Nil(t, err, "%+v", err)
+
+		assert.True(t, len(itemList.Items) <= len(nsList.Items))
+	}
+
+	{
+		_, err = adminSrv.DeleteNamespace(ctx, &metav1.DeleteOptions{Uid: ns.Metadata.Uid})
+		assert.Nil(t, err, "%+v", err)
+
+		itemList, err := usrSrv.ListNamespace(usrT.Ctx(), &userv1.ListNamespaceOptions{})
+		assert.Nil(t, err, "%+v", err)
+
+		for _, itm := range itemList.Items {
+			assert.NotEqual(t, ns.Metadata.Uid, itm.Metadata.Uid)
+		}
+	}
 }
