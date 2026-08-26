30c24555a108d7c1bcbfe3e3f6976f72c1fcaf2d
George B. <george@octelium.com>
2026-08-26 11:48:28 +0000

    cluster/portal - use @fontsource/ubuntu

--- cluster/portal/portal/web/package/src/main.tsx
+++ cluster/portal/portal/web/package/src/main.tsx
@@ -1,3 +1,6 @@
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
 import "@mantine/core/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
